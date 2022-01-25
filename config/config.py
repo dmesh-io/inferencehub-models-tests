6ed6ff9dd34a0f16f6a0f3dec24cf5aba0eb7b2e
moritz <moritz@dmesh.io>
2022-01-25 17:22:51 +0100

    Finished unet, started Yolov5

--- config/config.py
+++ config/config.py
@@ -9,6 +9,7 @@ class ApiConfig():
     """
 
     API_URL: str = "https://api.inferencehub.io/model"
+    # API_URL: str = "https://api-dev.inferencehub.io/model"
     # API_URL: str = "http://localhost:3000/model"    # For local testing with lambda
     # API_URL: str = "http://localhost:8080/model"    # For local testing with python app
 
--- utils.py
+++ utils.py
@@ -1,11 +1,32 @@
+import json
 from pathlib import Path
 from typing import Union
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as pltimg
+from skimage.transform import resize
+from skimage.io import imsave
 
-def show_image(path: Union[str, Path]):
+def show_image_from_path(path: Union[str, Path], size=(4, 4)):
     img = pltimg.imread(path)
-    plt.figure(figsize=[1, 1])
+    plt.figure(figsize=size)
     plt.imshow(img)
     plt.axis('off')
     plt.show()
+
+def show_image_from_numpy(array: np.array):
+    plt.imshow(array)
+    plt.axis('off')
+    plt.show()
+
+def show_image_from_response(bt_resp: bytes):
+    result = np.array(json.loads(bt_resp))
+    show_image_from_numpy(result)
+
+def resize_image(path: Union[str, Path], size=(512, 512)):
+    img = pltimg.imread(path)
+    img = resize(img, output_shape=size, order=0, anti_aliasing=False, preserve_range=True)
+    imsave("input_samples/image-yolo5.png", img)
+
+if __name__=="__main__":
+    resize_image("input_samples/image-yolo5.jpg", (512, 512))
